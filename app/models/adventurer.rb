826d78bdddfcfa1f1500b0c942770621be8af3e8
hideyuki matsuyama <choripon@gmail.com>
2019-09-12 21:50:57 +0900

    💚テストを書きやすくするためannotate gemを導入

--- app/models/adventurer.rb
+++ app/models/adventurer.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: adventurers
+#
+#  id                :bigint           not null, primary key
+#  agility           :integer          default(0), not null
+#  avoidance         :integer          default(0), not null
+#  concentration     :integer          default(0), not null
+#  intelligence      :integer          default(0), not null
+#  max_avoidance     :integer          default(0), not null
+#  max_concentration :integer          default(0), not null
+#  name              :string(255)      not null
+#  strength          :integer          default(0), not null
+#  toughness         :integer          default(0), not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 # 冒険者
 class Adventurer < ApplicationRecord
   validates :name, presence: true
